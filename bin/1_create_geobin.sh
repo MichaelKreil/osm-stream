e31ce7fd33e8a105d5699e5df2c469018c1dc453
Michael Kreil <github@michael-kreil.de>
2021-09-28 15:27:08 +0200

    rename data folders

---
+++ bin/1_create_geobin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+
+mkdir -r ../data/2_geobin/
+
+osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/liechtenstein-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/liechtenstein.geobuf.br
+osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/berlin-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/berlin.geobuf.br
+osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/germany-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/germany.geobuf.br
+osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/europe-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/europe.geobuf.br
