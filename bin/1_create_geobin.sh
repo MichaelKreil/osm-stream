c05c0c27aa8d5791790a2f4e6d015de82fc52276
Michael Kreil <github@michael-kreil.de>
2021-09-28 15:32:44 +0200

    fix parent mode

--- bin/1_create_geobin.sh
+++ bin/1_create_geobin.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname "$0")"
 
-mkdir -r ../data/2_geobin/
+mkdir -p ../data/2_geobin/
 
 osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/liechtenstein-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/liechtenstein.geobuf.br
 osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/berlin-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/berlin.geobuf.br
