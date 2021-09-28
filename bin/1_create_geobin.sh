0511f3223fae56941ae1f49b237e9174691c0526
Michael Kreil <github@michael-kreil.de>
2021-09-28 15:34:03 +0200

    switch to old syntax

--- bin/1_create_geobin.sh
+++ bin/1_create_geobin.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 mkdir -p ../data/2_geobin/
 
-osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/liechtenstein-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/liechtenstein.geobuf.br
-osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/berlin-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/berlin.geobuf.br
-osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/germany-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/germany.geobuf.br
-osmium export --progress -x print_record_separator=false -f geojsonseq ../data/1_osm/europe-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/europe.geobuf.br
+osmium export --progress -r -f geojsonseq ../data/1_osm/liechtenstein-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/liechtenstein.geobuf.br
+osmium export --progress -r -f geojsonseq ../data/1_osm/berlin-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/berlin.geobuf.br
+osmium export --progress -r -f geojsonseq ../data/1_osm/germany-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/germany.geobuf.br
+osmium export --progress -r -f geojsonseq ../data/1_osm/europe-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/europe.geobuf.br
