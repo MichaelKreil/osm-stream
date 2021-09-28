cdbc567de4746835868c3ce012fbc85c9185079f
Michael Kreil <github@michael-kreil.de>
2021-09-28 22:53:44 +0200

    better

--- bin/1_create_geobin.sh
+++ bin/1_create_geobin.sh
@@ -4,6 +4,11 @@ cd "$(dirname "$0")"
 
 mkdir -p ../data/2_geobin/
 
+wget -nc "https://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf"
+wget -nc "https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf"
+wget -nc "https://download.geofabrik.de/europe/germany-latest.osm.pbf"
+wget -nc "https://download.geofabrik.de/europe-latest.osm.pbf"
+
 osmium export --progress -r -f geojsonseq ../data/1_osm/liechtenstein-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/liechtenstein.geobuf.br
 osmium export --progress -r -f geojsonseq ../data/1_osm/berlin-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/berlin.geobuf.br
 osmium export --progress -r -f geojsonseq ../data/1_osm/germany-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/germany.geobuf.br
--- bin/geojson2geobin.js
+++ bin/geojson2geobin.js
@@ -6,17 +6,69 @@ const zlib = require('zlib');
 const river = require('mississippi-promise');
 river.split = river.split.default;
 
+const maxBufferSize = 16*1024*1024;
+
+let compressor = BrotliCompressor();
+let bufferSize = 0;
+
 river.pipe(
 	process.stdin,
 	river.split('\n'),
-	river.through(async function(chunk) {
+	river.through(
+		async function(chunk) {
+			chunk = chunk.toString()+'\n';
+			await compressor.write(chunk);
+			bufferSize += chunk.length;
+			//console.error('bufferSize', bufferSize);
 
-		let header = Buffer.allocUnsafe(4);
-		header.writeUInt32LE(chunk.length, 0);
-		this.push(header);
+			if (bufferSize < maxBufferSize) return
 
-		return chunk;
-	}),
-	zlib.createBrotliCompress({params:{ [zlib.constants.BROTLI_PARAM_QUALITY]:3 }}),
+			let result = await flush();
+			compressor = BrotliCompressor();
+			bufferSize = 0;
+			return result;
+		},
+		async function() {
+			if (bufferSize > 0) return await flush();
+		}
+	),
 	process.stdout
 )
+
+async function flush() {
+	console.error('flush');
+	let buffer = await compressor.dump();
+	let header = Buffer.allocUnsafe(4);
+	header.writeUInt32LE(buffer.length, 0);
+	console.error(buffer.length);
+
+	return Buffer.concat([header,buffer]);
+}
+
+function BrotliCompressor(quality = 5) {
+	console.error('BrotliCompressor');
+	let compressor = zlib.createBrotliCompress({
+		params:{
+			[zlib.constants.BROTLI_PARAM_QUALITY]:quality
+		}
+	});
+	let buffers = [];
+	let toBuffer = river.to(async (data,enc) => buffers.push(data));
+	compressor.pipe(toBuffer);
+
+	return { write, dump }
+
+	function write(chunk) {
+		return new Promise(res => {
+			if (compressor.write(chunk)) return res();
+			compressor.once('drain', () => res());
+		})
+	}
+
+	function dump(chunk) {
+		return new Promise(res => {
+			toBuffer.once('finish', () => res(Buffer.concat(buffers)));
+			compressor.end();
+		})
+	}
+}
