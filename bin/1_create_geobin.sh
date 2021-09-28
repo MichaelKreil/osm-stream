06870c38628476a2e6963587f7ad076ac66d7364
Michael Kreil <github@michael-kreil.de>
2021-09-29 00:23:59 +0200

    add planet

--- bin/1_create_geobin.sh
+++ bin/1_create_geobin.sh
@@ -4,12 +4,14 @@ cd "$(dirname "$0")"
 
 mkdir -p ../data/2_geobin/
 
-wget -nc "https://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf"
-wget -nc "https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf"
-wget -nc "https://download.geofabrik.de/europe/germany-latest.osm.pbf"
-wget -nc "https://download.geofabrik.de/europe-latest.osm.pbf"
+wget -nc -P ../data/1_osm "https://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf"
+wget -nc -P ../data/1_osm "https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf"
+wget -nc -P ../data/1_osm "https://download.geofabrik.de/europe/germany-latest.osm.pbf"
+wget -nc -P ../data/1_osm "https://download.geofabrik.de/europe-latest.osm.pbf"
+wget -nc -P ../data/1_osm "https://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-latest.osm.pbf"
 
 osmium export --progress -r -f geojsonseq ../data/1_osm/liechtenstein-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/liechtenstein.geobin
 osmium export --progress -r -f geojsonseq ../data/1_osm/berlin-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/berlin.geobin
 osmium export --progress -r -f geojsonseq ../data/1_osm/germany-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/germany.geobin
 osmium export --progress -r -f geojsonseq ../data/1_osm/europe-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/europe.geobin
+osmium export --progress -r -f geojsonseq ../data/1_osm/planet-latest.osm.pbf | node geojson2geobin.js > ../data/2_geobin/planet.geobin
